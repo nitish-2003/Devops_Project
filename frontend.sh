aba89ffa25c846e294447277c0717573ad87f13d
nitish-2003 <102981108+nitish-2003@users.noreply.github.com>
2024-08-08 13:06:12 +0530

    Create frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+docker run -d \
+    --name mysql \
+    -v mysql-data:/var/lib/mysql \
+    --network=twotier \
+    -e MYSQL_DATABASE=mydb \
+    -e MYSQL_ROOT_PASSWORD=admin \
+    -p 3306:3306 \
+    mysql:5.7
+
+sudo docker ps
+
+docker run -d \
+    --name flaskapp \
+    --network=twotier \
+    -e MYSQL_HOST=mysql \
+    -e MYSQL_USER=root \
+    -e MYSQL_PASSWORD=admin \
+    -e MYSQL_DB=mydb \
+    -p 5000:5000 \
+    nitishbehera/flaskapp:latest
+
+sudo docker ps
+
+sleep 15
+
+sudo docker ps
+docker rm flaskapp
+docker run -d \
+    --name flaskapp \
+    --network=twotier \
+    -e MYSQL_HOST=mysql \
+    -e MYSQL_USER=root \
+    -e MYSQL_PASSWORD=admin \
+    -e MYSQL_DB=mydb \
+    -p 5000:5000 \
+    nitishbehera/flaskapp:latest
+sleep 10
+sudo docker ps
